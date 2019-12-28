e3f264a5b5aaaa4ed11ddf5f6c351ee29b5200c0
whatwant <whatwant@gmail.com>
2019-12-28 15:45:58 +0900

    Update template.py

--- python/template.py
+++ python/template.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 #-*- encoding: utf-8 -*-
 
-
+import pprint
 import sys
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 if sys.version_info.major < 3:
     print( 'Upgrade to Python 3' )
     exit(1)
 
+pp = pprint.PrettyPrinter(indent=4)
 
 if __name__ == "__main__":
 
